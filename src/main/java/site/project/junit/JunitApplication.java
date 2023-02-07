73998057286168b53f7cb4c584c27afe8c10def7
hongxeob <sjun2918@naver.com>
2023-02-07 15:33:50 +0900

    git init

---
+++ src/main/java/site/project/junit/JunitApplication.java
@@ -0,0 +1,13 @@
+package site.project.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/project/junit/JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package site.project.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
